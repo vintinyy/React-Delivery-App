0e88cc64da14b0ee33089f80238250c6426e36ec
vintinyy <vintine.anrich@gmail.com>
2021-02-25 16:30:59 -0800

    App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,34 @@ export default function App() {
   let [total, setTotal] = useState(0);
   let [cart, setCart] = useState("");
   let [newProduct, setNewProduct] = useState("");
+  let [products, setProducts] = useState(
+    [
+      {
+        id:"1",
+        name:"Pizza",
+        price:"$1.50",
+        image:"https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/phut_0.jpg?itok=h30EAnkk",
+        description:"Dough baked with tomato sauce , cheese,  and pepperoni."
+      },
+      {
+        id:"2",
+        name:"Coffee",
+        price:"$2.50",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMXX9ZFy_phkiDpZDzg7md0eXIfUHwmqkAKg&usqp=CAU",
+        description:""
+      },
+      {
+        id:"3",
+        name:"Pasta",
+        price:"$5.00",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVZCciLeOCaMZvZB6n1qcIwRgUlKPzth2Pig&usqp=CAU",
+        description: ""
+      }
+    ]
+  )
+
+
+
   function addProductToCart(product, price) {
     let products = cart;
     products += product + ", ";
@@ -35,36 +63,24 @@ export default function App() {
       <h2>Cart : {cart}</h2>
       <button onClick={() => clearCart()}>Remove all Items </button>
       {items}
-      <ProductCard
-        id="1"
-        name="Pizza"
-        price="$1.50"
-        image="https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/phut_0.jpg?itok=h30EAnkk"
-        description="Dough baked with tomato sauce , cheese,  and pepperoni."
-      />
-      <button onClick={() => addProductToCart("Pizza", 1.5)}>
-        Add to Cart
-      </button>
 
-      <ProductCard
-        id="2"
-        name="Coffee"
-        price="$2.50"
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMXX9ZFy_phkiDpZDzg7md0eXIfUHwmqkAKg&usqp=CAU"
-        description=""
-      />
-      <button onClick={() => addProductToCart("Coffee", 1.0)}>
-        Add to Cart
-      </button>
-      <ProductCard
-        id="3"
-        name="Pasta"
-        price="$5.00"
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVZCciLeOCaMZvZB6n1qcIwRgUlKPzth2Pig&usqp=CAU"
-      />
-      <button onClick={() => addProductToCart("Pasta", 3.0)}>
-        Add to Cart
-      </button>
+      {products.map(item => {
+        return (
+          <div>
+            <ProductCard
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                image={item.image}
+              />
+              <button onClick={() => addProductToCart(item.name, item.price)}>
+                Add to Cart
+              </button>
+          </div>
+        )
+      })}
+     
+      
 
       <p>Start editing to see some magic happen :) </p>
     </div>
